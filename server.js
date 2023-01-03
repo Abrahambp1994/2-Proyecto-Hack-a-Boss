ab7cd5f71682197df7d1e8029d21b245ad36accc
Abrahambp1994 <abrahambp1994@icloud.com>
2023-01-03 14:39:32 +0100

    Insercion de tablas, carpetas, instalacion de modules

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+const env = require("dotenv").config();
 const express = require("express");
 const fileUpload = require("express-fileupload");
 
@@ -8,6 +8,8 @@ const { PORT } = process.env;
 
 app.use(express.json());
 app.use(fileUpload());
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
---
+++ src/database/populateDb.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const getPool = require("./getPool");
+
+
+const populateDb = async () => {
+  try {
+    const pool = getPool();
+
+    console.log("Inserting users...");
+
+    await pool.query(`
+        INSERT INTO users (email, password, name) VALUES 
+        ("Fernando@email.com", "${await bcrypt.hash(
+          "123456",
+          10
+        )}", "Fernando"),
+        ("Pilar@email.com", "${await bcrypt.hash(
+          "123456",
+          10
+        )}", "Pilar"),
+        ("Paula@email.com", "${await bcrypt.hash(
+          "123456",
+          10
+        )}", "Paula"),
+        ("Pablo@email.com", "${await bcrypt.hash(
+            "123456",
+            10
+          )}", "Pablo")
+    `);
+
+    console.log("Inserting posts...");
+
+    await pool.query(`
+        INSERT INTO posts (image, description, userId) VALUES 
+        ("", "Ruta por Gredos. Precioso día rodeado de amigos", 1),
+        ("", "Italia. Gran ciudad para disfrutar de sus monumentos", 2),
+        ("", "Formigal. Bonito lugar para hacer deporte y disfrutar de la naturaleza", 3),
+        ("", "Crucero por el Nilo. Impresionante viaje en barco", 4)
+
+    `);
+
+    console.log("Inserting likes...");
+
+    await pool.query(`
+        INSERT INTO likes (postId, userId) VALUES 
+        (3, 2),
+        (1, 3),
+        (2, 1),
+        (4, 4)
+    `);
+
+    console.log("¡Datos insertados! 🥳");
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    process.exit();
+  }
+};
+
+populateDb();
